45f4664af4edaa2b0480d3067eeef5b6f958353e
Arjun Varma <Arjun.varma@probeinformation.com>
2024-03-16 14:21:11 +0000

    Modified gps apis and added a few incomplete admin apis

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -25,19 +25,5 @@ app.config["JWT_SECRET_KEY"] = "VERY-SECRET-JWT-KEY"
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(app)
 
-def current_user_is_admin(user_id):
-    user = models.User.query.get(user_id)
-    return user and any(role.name == 'admin' for role in user.roles)
-
-def admin_required(fn):
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        verify_jwt_in_request()
-        current_user_id = get_jwt_identity()
-        if not current_user_is_admin(current_user_id):
-            return jsonify({"error": "Admin access required"}), 403
-        return fn(*args, **kwargs)
-    return wrapper
-
 # Importing all the modules
 from app import resources, models
