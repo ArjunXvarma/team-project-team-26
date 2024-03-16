1798cfb6b336917ca2b45d8d5bc3001059f3604b
Arjun Varma <Arjun.varma@probeinformation.com>
2024-03-16 16:40:02 +0000

    Fixed tests

--- backend/tests/test_project.py
+++ backend/tests/test_project.py
@@ -173,45 +173,39 @@ class TestGPSRoutes:
         response = client.put("/update_journey/1", json=journey_update_data)
         assert response.status_code == 401
 
-    def test_validate_points1(self):
-        """Test valid points"""
+    def test_validate_points(self):
         points_valid = [
             {'lat': 10, 'lon': 20, 'ele': 5},
             {'lat': 15, 'lon': 25, 'ele': 10}
         ]
         assert GPSRoutes.validate_points(points_valid)[0] == True
 
-    def validate_points2(self):
-        """Test points with missing keys"""
+    def validate_points_missing_keys(self):
         points_missing_key = [
             {'lat': 10, 'lon': 20, 'ele': 5},
             {'lat': 15, 'lon': 25}
         ]
         assert GPSRoutes.validate_points(points_missing_key)[0] == False
 
-    def validate_points3(self):
-        """Test points with extra keys"""
+    def validate_points_extra_keys(self):
         points_extra_key = [
             {'lat': 10, 'lon': 20, 'ele': 5},
             {'lat': 15, 'lon': 25, 'ele': 10, 'temp': 50} 
         ]
         assert GPSRoutes.validate_points(points_extra_key)[0] == False
 
-    def validate_points4(self):
-        """Test points with extra and missing keys"""
+    def validate_points_missing_and_extra_keys(self):
         points_missing_and_extra_keys = [
             {'lat': 10, 'lon': 20, 'ele': 5},
             {'lat': 15, 'temp': 50} 
         ]
         assert GPSRoutes.validate_points(points_missing_and_extra_keys)[0] == False
 
-    def validate_points5(self):
-        """Test empty points"""
+    def validate_points_empty(self):
         points_empty = []
         assert GPSRoutes.validate_points(points_empty)[0] == False
 
-    def validate_points6(self):
-        """Test different ordered points"""
+    def validate_points_different_order(self):
         points_different_order = [
             {'ele': 5, 'lon': 20, 'lat': 10}
         ]
