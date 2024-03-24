2e192aff6ca3013d3a90b8c882f6aadea3c3ec49
tahiralileeds <124058348+tahiralileeds@users.noreply.github.com>
2024-03-24 23:41:19 +0400

    Added Logout Button to Navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { ReactNode } from "react";
 import { FaGear } from "react-icons/fa6";
+import { MdLogout } from "react-icons/md";
 import { FaHome, FaMapMarkedAlt, FaUserFriends } from "react-icons/fa";
 
 interface NavbarProps {
@@ -39,6 +40,12 @@ export default function Navbar({ children }: NavbarProps) {
             </div>
           </Link>
         </div>
+        <Link href={"/logout"} prefetch={false} className=" mt-20">
+          <div className="text-red-500 flex gap-3 font-medium w-48 p-5 rounded-xl hover:bg-green-800">
+            <MdLogout size={32} />
+            <p className="text-xl">Logout</p>
+          </div>
+        </Link>
       </div>
       <div className="w-full"> {children}</div>
     </div>
