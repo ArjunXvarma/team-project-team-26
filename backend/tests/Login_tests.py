2880463544262e8b60a7aee0e57dad4b71a4d065
sc22jat1 <sc22jat@leeds.ac.uk>
2024-04-10 15:46:35 +0100

    modularised backend test file into seperate files

---
+++ backend/tests/Login_tests.py
@@ -0,0 +1,59 @@
+from app import models, app, db
+from datetime import datetime, timedelta
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token
+import pytest
+import constants
+from app.Client.GPS import GPSRoutes
+from app.Client.Auth import AuthenticationRoutes
+from app.Client.Friends import FriendshipRoutes
+from app.Client.Membership import MembershipRoutes
+from app.Client.Stats import StatisticsRoutes
+from revenuePrediction import generateFutureRevenueData
+from tests.TestUsers import users
+
+# Initialize bcrypt
+bcrypt = Bcrypt(app)
+
+class TestLogin:
+    """Class for testing user login functionality."""
+
+    def test_correct_credentials(self, client, clean_db):
+        """Test login with correct credentials."""
+        token = users.user2(self, client, clean_db)[0]
+
+        response = client.post("/login", json={
+            "email": "alice@example.com",
+            "password": "password"
+        })
+
+        assert response.status_code == 200
+        assert response.json['name'] == "Alice Smith"
+        assert response.json['return_code'] == 2
+
+    def test_incorrect_password(self, client, clean_db):
+        """Test login with incorrect password."""
+        token = users.user2(self, client, clean_db)[0]
+
+        response = client.post("/login", json={
+            "email": "alice@example.com",
+            "password": "pass word"
+        })
+
+        assert response.status_code == 401
+        assert response.json['error'] == "Incorrect password, please try again"
+        assert response.json['return_code'] == 1
+
+    def test_incorrect_email(self, client, clean_db):
+        """Test login with incorrect email."""
+        token = users.user2(self, client, clean_db)[0]
+
+
+        response = client.post("/login", json={
+            "email": "peter.parker.wrong.mail@gmail.com",
+            "password": "password"
+        })
+
+        assert response.status_code == 404
+        assert response.json['error'] == "User Not found with the given Email"
+        assert response.json['return_code'] == 0
