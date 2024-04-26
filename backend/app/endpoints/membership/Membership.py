ad990772dde870f00af8448b6674f59ffc506a06
shubham-bajaj15 <sc22sb@leeds.ac.uk>
2024-04-26 18:09:23 +0100

    "Made changes to membership APIs."

--- backend/app/endpoints/membership/Membership.py
+++ backend/app/endpoints/membership/Membership.py
@@ -17,10 +17,21 @@ class MembershipRoutes:
     -------
     buy_membership() -> json:
         Allows a user to purchase a membership.
+
+    update_membership() -> json:
+        Updates the user's membership with specified details.
+
     cancel_membership() -> json:
         Cancels the auto renewal of membership of a user.
+
     has_active_membership() -> bool:
-        Returns if an user have an active membership or not.
+        Returns if a user has an active membership or not.
+
+    next_billing_cycle_date() -> json:
+        Retrieves the next billing cycle date for the user's active membership.
+    
+    get_current_membership() -> json:
+        Returns the details about the active user membership
     """
 
     @app.route("/buy_membership", methods=["POST"])
@@ -34,7 +45,7 @@ class MembershipRoutes:
         membership_type : str
             Type of membership to purchase (All types defined in constants.py).
         duration : str
-            Duration of the membership (All valid durations defined in constants.py)..
+            Duration of the membership (All valid durations defined in constants.py).
         mode_of_payment : str
             Mode of payment for the membership (All modes defined in constants.py).
 
@@ -140,7 +151,7 @@ class MembershipRoutes:
         if not membership:
             return jsonify({"return_code": 0, "error": "User does not have an active membership"}), 404
 
-        current_date = datetime.utcnow()
+        current_date = datetime.now()
         if current_date < membership.end_date:
             membership.auto_renew = False
             message = "Auto-renew disabled successfully."
@@ -152,6 +163,166 @@ class MembershipRoutes:
         db.session.commit()
 
         return jsonify({"return_code": 1, "message": message}), 200
+
+    @app.route("/update_membership", methods=["POST"])
+    @jwt_required()
+    def update_membership():
+        """
+        Update the user's membership with specified details.
+
+        Parameters
+        ----------
+        membership_type : str
+            Type of membership to update to (All types defined in constants.py).
+        duration : str
+            New duration of the membership (All valid durations defined in constants.py).
+        auto_renew : bool, optional
+            Flag indicating whether the membership should auto-renew (default is False).
+
+        Returns
+        -------
+        json
+            A JSON response indicating the success or failure of the membership update.
+            If successful, returns:
+                - "return_code": 1
+                - "message": "Membership update scheduled successfully"
+            If unsuccessful, returns:
+                - "return_code": 0
+                - "error": Details about the encountered error during membership update.
+
+        HTTP Status Codes
+        -----------------
+        200 : OK
+            Membership update scheduled successfully.
+        400 : Bad Request
+            - Missing required fields.
+            - Invalid membership type or duration.
+            - No active membership found for the user.
+        401 : Unauthorized
+            Missing or invalid access token.
+        """
+        current_user_email = get_jwt_identity()
+        user = models.User.query.filter_by(email=current_user_email).first()
+        data = request.json
+
+        if not data:
+            return jsonify({"return_code": 0, "error": "No JSON Data found"}), 400
+
+        membership_type = data.get("membership_type")
+        duration = data.get("duration")
+        if not all([membership_type, duration]):
+            return jsonify({"return_code": 0, "error": "Missing Required Fields"}), 400
+
+        # Check for active membership
+        current_membership = models.Membership.query.filter_by(user_id=user.id, is_active=True).first()
+        if not current_membership:
+            return jsonify({"return_code": 0, "error": "No active membership found"}), 400
+
+        # Validate membership type and duration
+        if not constants.is_valid_membership_type(membership_type):
+            return jsonify({"return_code": 0, "error": "Invalid membership type"}), 400
+        if not constants.is_valid_duration(duration):
+            return jsonify({"return_code": 0, "error": "Invalid duration"}), 400
+
+        # Create pending update
+        pending_update = models.PendingMembershipUpdate(
+            user_id=user.id,
+            membership_type=membership_type,
+            duration=duration,
+            auto_renew=data.get("auto_renew", False) 
+        )
+        current_membership.auto_renew = True
+        db.session.add(pending_update)
+        db.session.commit()
+        return jsonify({"return_code": 1, "message": "Membership update scheduled successfully and auto renew is turned on"}), 200
+
+    @app.route("/get_current_membership", methods=["GET"])
+    @jwt_required()
+    def get_current_membership() -> Tuple[Response, int]:
+        """
+        Retrieves the details of the user's current active membership.
+
+        Returns
+        -------
+        json
+            A JSON response containing the details of the current membership.
+            If user has an active membership, returns:
+                - "membership_type": Type of the membership (All types defined in constants.py).
+                - "start_date": Start date of the membership in ISO format (e.g., "2024-04-26").
+                - "end_date": End date of the membership in ISO format (e.g., "2025-04-26").
+            If user does not have an active membership, returns:
+                - "message": "User does not have an active membership."
+
+        HTTP Status Codes
+        -----------------
+        200 : OK
+            Membership details retrieved successfully.
+        401 : Unauthorized
+            Missing or invalid access token.
+        """
+        current_user_email = get_jwt_identity()
+        if not current_user_email:
+            return jsonify({"message": "Unauthorized"}), 401
+
+        user = models.User.query.filter_by(email=current_user_email).first()
+        if not user:
+            return jsonify({"message": "User not found"}), 404
+
+        # Check if user has an active membership
+        active_membership = models.Membership.query.filter_by(user_id=user.id, is_active=True).first()
+
+        if active_membership:
+            membership_type = active_membership.membership_type
+            start_date = active_membership.start_date.isoformat()
+            end_date = active_membership.end_date.isoformat()
+            membership_duration = active_membership.duration
+            auto_renew = active_membership.auto_renew
+            mode_of_payment = active_membership.mode_of_payment
+            return jsonify({
+                "membership_type": membership_type,
+                "membership_duration":membership_duration,
+                "mode_of_payment":mode_of_payment,
+                "start_date": start_date,
+                "end_date": end_date,
+                "auto_renew": auto_renew,
+            }), 200
+        else:
+            return jsonify({"message": "User does not have an active membership."}), 404
+
+    @app.route("/get_billing_cycle_date", methods=["GET"])
+    @jwt_required()
+    def get_billing_cycle_date() -> Tuple[Response, int]:
+        """
+        Retrieves the next billing cycle date for the user's active membership.
+
+        Returns
+        -------
+        json
+            A JSON response containing the next billing cycle date.
+            If user has an active membership, returns:
+                - "next_billing_cycle_date": Date of the next billing cycle in ISO format (e.g., "2024-05-26").
+            If user does not have an active membership, returns:
+                - "next_billing_cycle_date": null
+
+        HTTP Status Codes
+        -----------------
+        200 : OK
+            Next billing cycle date retrieved successfully.
+        401 : Unauthorized
+            Missing or invalid access token.
+        """
+        current_user_email = get_jwt_identity()
+        user = models.User.query.filter_by(email=current_user_email).first()
+
+        # Check if user has an active membership
+        active_membership = models.Membership.query.filter_by(user_id=user.id, is_active=True).first()
+
+        if active_membership:
+            next_billing_cycle_date = active_membership.end_date
+            return jsonify({"next_billing_cycle_date": next_billing_cycle_date.isoformat()}), 200
+        else:
+            return jsonify({"next_billing_cycle_date": None}), 200
+
 
     @app.route("/has_active_membership", methods=["GET"])
     @jwt_required()
@@ -186,37 +357,10 @@ class MembershipRoutes:
         else:
             return jsonify({"has_active_membership": False}), 200
 
-    def auto_renew_memberships():
-        """
-        Function to auto-renew memberships if today is the end date and auto renew is True.
-        """
-        current_date = datetime.utcnow()
-
-        memberships_to_renew = models.Membership.query.filter(models.Membership.end_date == current_date, models.Membership.auto_renew == True).all()
-
-        for membership in memberships_to_renew:
-            if membership.duration.lower() == 'monthly':
-                membership.end_date += timedelta(days=30)
-            elif membership.duration.lower() == 'annually':
-                membership.end_date += timedelta(days=365)
-        db.session.commit()
-
-    def deactivate_expired_memberships():
-        """
-        Function to deactivate memberships if today is the end date and auto renew is False.
-        """
-        current_date = datetime.utcnow()
-
-        memberships_to_deactivate = models.Membership.query.filter(models.Membership.end_date == current_date, models.Membership.auto_renew == False).all()
-
-        for membership in memberships_to_deactivate:
-            membership.is_active = False
-
-        db.session.commit()
 
 
 # Running the auto renew and deactivation functions every day using scheduler.
 scheduler = BackgroundScheduler()
-scheduler.add_job(MembershipRoutes.auto_renew_memberships, 'cron', hour=16, minute=0)
-scheduler.add_job(MembershipRoutes.deactivate_expired_memberships, 'cron', hour=16, minute=0)
+scheduler.add_job(models.Membership.auto_renew_memberships, 'cron', hour=16, minute=0)
+scheduler.add_job(models.Membership.deactivate_expired_memberships, 'cron', hour=16, minute=0)
 scheduler.start()
