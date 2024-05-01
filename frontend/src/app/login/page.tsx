cc27844c98eed4e3221cd60b5734a1720feff0f3
darkpoison7 <tahiru5253@gmail.com>
2024-05-01 13:57:40 +0100

    Added dark mode to login page and made it responsive.

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import "./loginStyles.css";
 import Link from "next/link";
 import Image from "next/image";
 import Cookie from "js-cookie";
@@ -6,12 +7,15 @@ import { useState } from "react";
 import { API_URL } from "@/constants";
 import { useForm } from "@mantine/form";
 import { useRouter } from "next/navigation";
-import { PasswordInput, Button, Divider, TextInput } from "@mantine/core";
-import { showErrorMessage, showSuccessMessage } from "@/utils";
 import { CheckAdminAPIResponse } from "@/types";
+import { CiLock, CiUser } from "react-icons/ci";
+import { useTheme } from "@/components/theme-provider";
+import { showErrorMessage, showSuccessMessage } from "@/utils";
+import { PasswordInput, Button, Divider, TextInput } from "@mantine/core";
 
 export default function Login() {
   const router = useRouter();
+  const { theme } = useTheme();
   const [loading, setLoading] = useState(false);
 
   const form = useForm({
@@ -104,28 +108,70 @@ export default function Login() {
             <p className="flex text-green-900 justify-center self-center font-serif text-xl font-semibold">FitFusion</p>
           </div>
           <Image src="/RunMan.png" alt="Runner Image" width={400} height={300} />
+      <div
+        className={`flex md:flex-row flex:col w-full h-full items-center md:gap-10 bg-[${
+          theme == "dark" ? "#131B23" : "#F1F1F1"
+        }]`}
+      >
+        <div className="w-5/6 h-screen md:flex items-center justify-center hidden">
+          <div
+            className={`appname-container flex items-center justify-center ${
+              theme == "dark" ? "gradient--dark-mode" : ""
+            }`}
+          >
+            <h1
+              className={`text-xl font-semibold text-${
+                theme == "dark" ? "white" : "[#043c31]"
+              } text-center leading-10 font-serif`}
+            >
+              Fit Fusion
+            </h1>
+          </div>
+          <div>
+            <Image src="/runner-1.png" alt="Runner Image" width={550} height={380} />
+          </div>
         </div>
         <div className="w-full flex items-center justify-center m-6 rounded-3xl" style={gradient}>
           <div className="w-96 flex flex-col items-center gap-10">
             <h1 className="text-4xl font-black font-serif text-white">Login</h1>
             <form className="w-full flex flex-col gap-3 items-center">
+        <div
+          className={`h-screen md:h-[calc(100vh-40px)] w-full md:w-3/5 flex items-center justify-center md:rounded-3xl md:mr-5 ${
+            theme == "dark" ? "gradient--dark-mode" : "gradient--light-mode"
+          }`}
+        >
+          <div className="w-96 flex flex-col items-center gap-10 px-10">
+            <h1 className="font-serif text-4xl font-semibold leading-10 text-center text-white">
+              Login
+            </h1>
+            <form className="w-full flex flex-col gap-10 items-center">
               <TextInput
+                size="lg"
                 type="email"
                 placeholder="Email"
-                className="w-full text-white"
                 {...form.getInputProps("email")}
+                leftSection={<CiUser size={32} />}
+                className={`w-full login ${theme == "dark" ? "input--dark-mode" : ""}`}
               />
               <PasswordInput
                 className="w-full mt-4"
+                size="lg"
+                id="login-pwd"
                 placeholder="Password"
+                leftSection={<CiLock size={32} />}
                 {...form.getInputProps("password")}
+                className={`w-full login ${theme == "dark" ? "input--dark-mode" : ""}`}
               />
               <div className="flex flex-col justify-center gap-3 mt-10 w-48">
                 <Button
+                  color="green"
                   onClick={submit}
                   loading={loading}
                   className="bg-green-700 h-10 text-lg rounded-2xl w-full"
                   style={{ backgroundColor: "rgb(51, 192, 116, 1)" }}
+                  className={`login-button ${
+                    theme == "dark" ? "login-button--dark-mode" : ""
+                  }`}
                 >
                   Login
                 </Button>
