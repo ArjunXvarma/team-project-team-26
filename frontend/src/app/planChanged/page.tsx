e89452c43f1e7427ae326e1bfa43e10bba4338e8
darkpoison7 <tahiru5253@gmail.com>
2024-05-07 17:33:08 +0100

    Added dark mode and responsiveness to Plan Changes page.

--- frontend/src/app/planChanged/page.tsx
+++ frontend/src/app/planChanged/page.tsx
@@ -1,35 +1,52 @@
-"use client"
+"use client";
 import Link from "next/link";
-import Cookies from "js-cookie";
-import { useState } from 'react';
 import { Image } from "@mantine/core";
 import { Button } from "@mantine/core";
-import { IoMdHome } from "react-icons/io";
 import { IoFootsteps } from "react-icons/io5";
+import { useTheme } from "@/components/theme-provider";
 
 export default function Thankyou() {
-    const [username, setUsername] = useState(Cookies.get("username"));
-     
-    return ( 
-        <main className="w-screen h-screen bg-tertiary relative">
-            <div className="flex justify-center p-28 "> 
-                <div className="bg-white rounded-lg shadow-xl flex-col text-center p-28  relative z-10"> 
-                    <p className=" text-4xl font-semibold text-green-800">Success!</p>
-                    <p className=" text-2xl font-semibold mt-8">Your membership plans have been updated.</p>
-                    <p className=" text-2xl font-semibold mt-2">You will be charged from the same payment method as before, after current plan expires.</p>
-                    <p className=" text-3xl font-semibold mt-8">View your dashboard</p>
-                    <Link href={'/dashboard'}>
-                        <Button className="mt-5 bg-green-700" color="green" size="md"><IoMdHome size={36}/></Button>
-                    </Link>
-                    <IoFootsteps size={150} className="absolute top-2 right-0 transform translate-x-1/2 -translate-y-1/2 -rotate-45"/>
-                </div>
+  const { theme } = useTheme();
+  return (
+    <main
+      className={`w-screen h-screen relative ${
+        theme == "dark" ? "bg-dk_background" : "bg-tertiary"
+      }`}
+    >
+      <div className="flex justify-center p-28 ">
+        <div
+          className={`rounded-3xl shadow-xl flex-col text-center p-28 relative z-10 ${
+            theme == "dark" ? "bg-[#1B2733] text-white" : "bg-white"
+          }`}
+        >
+          <p className=" text-4xl font-semibold">Success!</p>
+          <p className=" text-4xl font-semibold mt-4 mb-20">
+            Your membership plans have been updated.
+          </p>
+          <div className="mt-5">
+            <Link href={"/dashboard"}>
+              <Button
+                className="text-white gradient--dark-mode"
+                style={{
+                  background:
+                    "linear-gradient(180deg, #2f7149 0%, #044e40 89.81%, #043429 100%);",
+                }}
+              >
+                View dashboard
+              </Button>
+            </Link>
+          </div>
+          <IoFootsteps
+            size={150}
+            color={theme == "dark" ? "#5FE996" : "#000"}
+            className="absolute top-2 right-0 transform translate-x-1/2 -translate-y-1/2 -rotate-45"
+          />
+        </div>
 
-                 <div className="absolute bottom-0 right-0 z-0">
-                    <Image src="/plant2.png" alt="Plant image" className="w-screen"/>
-                </div>
-            </div>
-           
-        </main>
-     );
+        <div className="absolute bottom-0 left-0 right-0 z-0">
+          <Image src="/plant2.png" alt="Plant image" width={"100vw"} />
+        </div>
+      </div>
+    </main>
+  );
 }
-
