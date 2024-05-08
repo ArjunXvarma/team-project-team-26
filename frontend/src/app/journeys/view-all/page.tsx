4258f7fa304daaef6564eedf4699436bfd4e816f
darkpoison7 <tahiru5253@gmail.com>
2024-05-08 01:05:20 +0100

    Bug Fixes

--- frontend/src/app/journeys/view-all/page.tsx
+++ frontend/src/app/journeys/view-all/page.tsx
@@ -10,20 +10,13 @@ import { showErrorMessage } from "@/utils";
 import { GetJourneyAPIResponse } from "@/types";
 import { useTheme } from "@/components/theme-provider";
 import React, { useEffect, useMemo, useState } from "react";
+import { Circle, MapContainer, Polyline, TileLayer } from "react-leaflet";
 
 export default function AllJourney() {
   const { theme } = useTheme();
   const [journeys, setJourneys] = useState<Journey[]>([]);
   const [getJourneyLoading, setGetJourneyLoading] = useState(true);
   const [legend, setLegend] = useState<{ color: string; name: String }[]>([]);
-  const Map = useMemo(
-    () =>
-      dynamic(() => import("@/components/map/Map"), {
-        ssr: false,
-        loading: () => <p>A map is loading</p>,
-      }),
-    []
-  );
 
   const getRandomColor = () => {
     return "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0");
@@ -101,18 +94,47 @@ export default function AllJourney() {
                   {journeys && journeys.length > 0 ? (
                     <div className="w-full flex md:flex-row items-center md:items-start flex-col justify-center">
                       <div className="mt-3 h-2/3 md:w-2/3 w-full md:mx-3 mx-5">
-                        <Map
-                          tracks={journeys.map((journey, i) => ({
-                            data: journey.points,
-                            color: legend[i].color,
-                          }))}
-                          center={{
-                            lat: journeys[0].points[Math.ceil(journeys[0].points.length / 2)]
-                              .lat,
-                            lng: journeys[0].points[Math.ceil(journeys[0].points.length / 2)]
-                              .lon,
-                          }}
-                        />
+                        <MapContainer
+                          center={[
+                            journeys[0].points[Math.ceil(journeys[0].points.length / 2)].lat,
+                            journeys[0].points[Math.ceil(journeys[0].points.length / 2)].lon,
+                          ]}
+                          zoom={16}
+                          scrollWheelZoom={true}
+                          className="w-full h-[700px]"
+                        >
+                          <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                          />
+                          {journeys.map((journey, i) => (
+                            <div key={i}>
+                              <Polyline
+                                pathOptions={{ color: legend[i].color, weight: 5 }}
+                                positions={journey.points.map((point) => [
+                                  point.lat,
+                                  point.lon,
+                                ])}
+                              />
+                              <Circle
+                                radius={0}
+                                weight={8}
+                                color="#c90028"
+                                center={[journey.points[0].lat, journey.points[0].lon]}
+                              />
+                              <Circle
+                                radius={0}
+                                weight={8}
+                                fill={true}
+                                color="#005dba"
+                                center={[
+                                  journey.points[journey.points.length - 1].lat,
+                                  journey.points[journey.points.length - 1].lon,
+                                ]}
+                              />
+                            </div>
+                          ))}
+                        </MapContainer>
                       </div>
                       <div
                         className={`flex flex-col gap-3 rounded-2xl px-10 py-3 relative top-0 left-0 h-fit md:mt-0 mt-10 md:w-fit w-full drop-shadow-sharp ${
